bc9190bd9dbec469e8d0ebe4b825ae6ba4bcdefd
Marco Medhat <marco.medhat@webkeyz.com>
2021-05-28 13:03:01 +0200

    feat: create VideoDetails component with player and related videos

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -12,7 +12,10 @@
         </div>
         <div class="card video" v-if="searchResult.id.kind == 'youtube#video'">
           <div class="card-image">
-            <img :src="searchResult.snippet.thumbnails.default.url" :alt="searchResult.snippet.description">
+            <router-link :to="`/video/${searchResult.id.videoId}`">
+                        <img :src="searchResult.snippet.thumbnails.default.url" :alt="searchResult.snippet.description">
+
+            </router-link>
           </div>
           <div class="card-details">
             <h3>{{searchResult.snippet.title}}</h3>
@@ -29,26 +32,39 @@
 
 export default {
   name: 'Home',
+  props: ['videoId'],
+  watch: {
+    videoId: {
+      immediate: true, 
+      handler (val) {
+        this.relVideoId = val;
+      }
+    }
+  },
   data() {
     return {
       searchText: this.$route.query.query,
       url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBObo-4LwV58IPz2qzPBHkrlBnYlBCLpog',
       pageToken: '',
-      searchResults: []
+      searchResults: [],
+      relVideoId: ''
     }
   },
   created() {
-    console.log(this.searchText);
-    this.url += `&q=${this.searchText}`;
-      this.searchResults = [];
-      this.$http.get(this.url)
-      .then(data => {
-        console.log(data);
-        this.pageToken = data.body.nextPageToken;
-        data.body.items.forEach(element => {
-          this.searchResults.push(element);
-        });
+    if (this.videoId && this.videoId.length > 0) {
+      this.url += `&relatedToVideoId=${this.relVideoId}&type=video`
+    } else if (this.searchText.length > 0) {
+      this.url += `&q=${this.searchText}`;
+    }
+    this.searchResults = [];
+    this.$http.get(this.url)
+    .then(data => {
+      console.log(data);
+      this.pageToken = data.body.nextPageToken;
+      data.body.items.forEach(element => {
+        this.searchResults.push(element);
       });
+    });
   },
   methods: {
     loadMore: function() {
---
+++ src/components/VideoDetails.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="video">
+    <div class="video-container">
+       <iframe width="100%" height="270" :src='`//www.youtube.com/embed/${videoId}`' 
+       frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+       allowfullscreen></iframe>
+    </div>
+    <div class="video-details">
+      <h3 class="video-title">{{videoTitle}}</h3>
+      <h4 class="channel-title">{{channelTitle}} - <span>{{views}} Views</span></h4>
+    </div>
+    <Search :videoId="videoId"></Search> 
+  </div>
+</template>
+
+<script>
+import Search from './Search.vue'
+
+export default {
+  name: 'VideoDetails',
+  components: {
+    Search
+  },
+  data() {
+    return {
+      videoId: this.$route.params.id,
+      videoTitle: '',
+      channelTitle: '',
+      channelId: '',
+      views: ''
+    }
+  },
+  created() {
+    console.log(this.videoId);
+    this.$http.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&id=${this.videoId}&key=AIzaSyBObo-4LwV58IPz2qzPBHkrlBnYlBCLpog`)
+      .then(data => {
+        console.log(data);
+        this.videoTitle = data.body.items[0].snippet.title;
+        this.channelTitle = data.body.items[0].snippet.channelTitle;
+        this.channelId = data.body.items[0].snippet.channelId;
+        this.views = data.body.items[0].statistics.viewCount;
+      });
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.video {
+  padding: 2px;
+}
+  
+</style>
